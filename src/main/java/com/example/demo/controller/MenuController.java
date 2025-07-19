89043e0cedb6b266f428fda71f14ccdfa44068d8
DESKTOP-7V3A04U\DFran <dfrankmireles@outlook.com>
2025-07-19 01:34:06 -0600

    se cambia para aceptar model no dto

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -1,29 +1,27 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.MenuDTO;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class MenuController {
 
-    @GetMapping("/menu")
-    public List<MenuDTO> getMenu() {
-        return List.of(
+    @GetMapping("/mainpage")
+    public String renderMenuPage(Model model) {
+        List<MenuDTO> menu = List.of(
             new MenuDTO(1000L, "Administración", null, null, -1L, true, List.of(
                 new MenuDTO(1001L, "Candado Poliza Nomina", null, "polizaNominaConsulta", 1000L, true, null),
                 new MenuDTO(1100L, "Usuarios", null, "users", 1000L, true, List.of(
                     new MenuDTO(1110L, "Crear Usuario", null, "userInsertForm", 1100L, true, null)
-                )),
-                new MenuDTO(1200L, "Perfiles", null, "profiles", 1000L, true, List.of(
-                    new MenuDTO(1210L, "Crear Perfil", null, "profileInsertForm", 1200L, true, null)
-                )),
-                new MenuDTO(1400L, "Restaura Proceso Global", null, "restauraProcesoGlobal", 1000L, true, null)
-            ))
-        );
+                )))));
+        model.addAttribute("menuData", menu);
+        return "mainpage";
     }
 }
--- src/main/java/com/example/demo/models/MenuDTO.java
+++ src/main/java/com/example/demo/models/MenuDTO.java
@@ -2,7 +2,6 @@ package com.example.demo.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
